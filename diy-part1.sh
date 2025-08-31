ff419b81e933d32df76de1025963d4000fff595e
GruntFish <fbyu7@qq.com>
2025-08-31 18:59:08 +0800

    Create diy-part1.sh

---
+++ diy-part1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Uncomment a feed source
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# sed -i 's/KERNEL_PATCHVER:=6.12/KERNEL_PATCHVER:=6.6/' target/linux/qualcommax/Makefile
+
+cd package
+# git clone https://github.com/xiaorouji/openwrt-passwall-packages
+# git clone https://github.com/xiaorouji/openwrt-passwall
+
+git clone https://github.com/lwb1978/openwrt-gecoosac
+git clone https://github.com/pymumu/openwrt-smartdns smartdns/
+git clone https://github.com/pymumu/luci-app-smartdns 
